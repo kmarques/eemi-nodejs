d1e40d2b3f71c1961f7cd519b7b9534f47ebba77
Karl MARQUES <karl@kmarques.dev>
2025-04-02 12:29:21 +0200

    correction TP

--- scrapping/scrapping-html.js
+++ scrapping/scrapping-html.js
@@ -5,10 +5,11 @@
  * V0 : récupérer les codes http + leur message sous la forme
  *  [ { code: 200, message: "OK" } ]
  * V1 : Sauvegarder dans un fichier CSV
+ * code;message
+ * 200;OK
  */
 
-const URL =
-  "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/sprites/895.png";
+const URL = "https://fr.wikipedia.org/wiki/Liste_des_codes_HTTP";
 
 const https = require("node:https");
 const fs = require("node:fs/promises");
@@ -35,16 +36,37 @@ const request = https.request(URL, {}, function (response) {
     } else if (
       response.headers["content-type"].match(/(application|text)\/html/i)
     ) {
-      //
+      parsedData = new JSDOM(aggregatedData).window.document;
     } else {
       parsedData = aggregatedData;
     }
 
     // processing
-    /* insert here algo */
+    const cells = parsedData.querySelectorAll(
+      "table tr:not(:first-child) th, table tr:not(:first-child) td:first-of-type"
+    );
+    const processedData = [];
+    for (let i = 0; i < cells.length; i += 2) {
+      processedData.push({
+        code: parseInt(cells[i].textContent.trim()),
+        message: cells[i + 1].textContent.trim(),
+      });
+    }
 
     // save
-    /* insert here algo */
+    const result =
+      Object.keys(processedData[0])
+        .map((key) => `"${key}"`)
+        .join(";") +
+      "\n" +
+      processedData
+        .map((item) =>
+          Object.values(item)
+            .map((val) => (typeof val === "string" ? `"${val}"` : val))
+            .join(";")
+        )
+        .join("\n");
+    fs.writeFile("codes.csv", result);
   });
 });
 
